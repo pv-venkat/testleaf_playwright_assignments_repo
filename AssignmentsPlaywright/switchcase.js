49347d1c4b98a0afbd294942c8d1588fce6f5943
pv-venkat <venkat.manyum@gmail.com>
2025-09-18 23:41:17 +0530

    Add files via upload
    switch case assignment added

---
+++ AssignmentsPlaywright/switchcase.js
@@ -0,0 +1,23 @@
+let grade = 'B';
+function indiaelection()
+{
+    switch (grade) {
+        case 'A':
+            console.log("Student passed with First Class");     
+            break;
+        case 'B':
+            console.log("Student passed with Second Class");     
+            break;
+        case 'C':
+            console.log("Student passed with Third Class");     
+            break;
+         case 'D':
+            console.log("Student Failed");     
+            break;
+              
+          default:
+            console.log("Invalid grade");     
+            break;
+    }
+}
+    indiaelection();
