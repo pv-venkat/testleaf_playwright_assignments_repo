cb29af208db9232c78de500cf6ec74078f14b57d
pv-venkat <venkat.manyum@gmail.com>
2025-09-18 23:28:38 +0530

    Add files via upload
    Daily homework.

---
+++ AssignmentsPlaywright/datatypes.js
@@ -0,0 +1,14 @@
+
+    var firstname = "Venkat";
+    var companyname = "Testleaf";
+    var mobilenumber = 1709170925;
+   
+    console.log("First Name of the employee is :",  firstname );
+    console.log("Data Type of the firstname variable is",  typeof firstname);
+
+    console.log("Company Name of the employee is :",  companyname );
+    console.log("Data Type of the companyname variable is",  typeof companyname);
+
+    console.log("Mobile Number of the employee is :",  mobilenumber );
+    console.log("Data Type of the mobilenumber variable is",  typeof mobilenumber);    
+
---
+++ AssignmentsPlaywright/oddeven.js
@@ -0,0 +1,29 @@
+let a=-3;
+function oddeven ()
+{
+    if (a %2 == 0)
+    {
+        console.log("Even number");
+        
+    }
+    else 
+        console.log("Odd number");
+        
+    }
+
+oddeven();
+
+function oddevennumbers (x)
+{
+    if (x/2)
+    {
+        console.log("Even number");
+        
+    }
+    else 
+        console.log("Odd number");
+        
+    }
+
+oddevennumbers(8);
+
