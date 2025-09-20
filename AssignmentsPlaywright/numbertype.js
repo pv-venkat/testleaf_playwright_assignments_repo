c4abf75820f85343fab048c4c857c9b77b425cdc
pv-venkat <venkat.manyum@gmail.com>
2025-09-20 18:54:38 +0530

    Add files via upload

---
+++ AssignmentsPlaywright/numbertype.js
@@ -0,0 +1,18 @@
+function numbertype(num) 
+{
+    if (num > 0)   
+         {
+        console.log("The number is Positive");
+        }
+        else if (num < 0) {
+        console.log("The number is Negative");         
+        }
+        else 
+        {
+        console.log("The number is Zero");         
+
+        }
+}
+let num = '-24';
+numbertype(num);
+
