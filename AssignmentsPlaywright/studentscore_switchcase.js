9805c1d567b5c377ac080b09bd78c787df8e26a9
pv-venkat <venkat.manyum@gmail.com>
2025-09-20 22:50:13 +0530

    Add files via upload

---
+++ AssignmentsPlaywright/studentscore_switchcase.js
@@ -0,0 +1,25 @@
+function student() {
+let score = ">60";
+    switch (score) {
+        case ">75":
+            console.log("Student has passed with A grade");
+            break;
+        case ">60":
+            console.log("Student has passed with B grade");
+            break;
+        case ">50":
+            console.log("Student has passed with C grade");
+            break;
+        case ">40":
+            console.log("Student has passed with D grade");
+            break;
+         case "<30":
+            console.log("Student has failed with E grade");
+            break;
+        default:
+             return "Invalid marks."
+            break;
+    }
+}
+student();
+
